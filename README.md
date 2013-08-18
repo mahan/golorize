672f663cba10b1fc6b5dc76bde80a81a1189b4ef
Mattias Hansson <mattias.hz.hansson@gmail.com>
2013-08-18 12:33:08 +0200

    first version

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 golorize
 ========
 
-Ansi code markup (mini) lib for Go, Golang
+Ansi code markup (mini) lib for Go, Golang.
+
+installation
+============
+
+  go get github.com/mahan/golorize
+
+usage
+=====
+
+  println(golorize.Ansify("I #green[heard you liked #blink[tagging], so I added #yellow[tags to #bold[tags]]] so you can #magenta[tag while you #blue[tag]]"))
---
+++ golorize.go
@@ -0,0 +1,59 @@
+/*
+  Go adaptation of Colorize lib for Node.JS: https://github.com/mattpat/colorize
+*/
+
+package golorize
+
+import (
+  "regexp"
+  "strings"
+)
+
+var ansicodes map[string]string = map[string]string{
+  "reset": "\033[0m",
+  "bold": "\033[1m",
+  "italic": "\033[3m",
+  "underline": "\033[4m",
+  "blink": "\033[5m",
+  "black": "\033[30m",
+  "red": "\033[31m",
+  "green": "\033[32m",
+  "yellow": "\033[33m",
+  "blue": "\033[34m",
+  "magenta": "\033[35m",
+  "cyan": "\033[36m",
+  "white": "\033[37m",
+}
+
+var tag *regexp.Regexp
+var Enabled bool = true
+
+func Ansify(str string) string {
+  cstack := make([]string, 3)
+
+  for tag.MatchString(str) {
+    matches := tag.FindStringSubmatch(str)
+    orig := matches[0]
+
+    if Enabled {
+      if orig == "]" {
+        cstack = cstack[:len(cstack)-1]
+      } else {
+        name := matches[1]
+        if code, exists := ansicodes[name]; exists {
+          cstack = append(cstack, code)
+        }
+
+      }
+      //str = str.replace(orig, this.ansicodes.reset + cstack.join(''));
+      str = strings.Replace(str, orig, ansicodes["reset"] + strings.Join(cstack, ""), -1)
+    } else {
+      str = strings.Replace(str, orig, "", -1)
+    }
+  } //for
+  return str
+}
+
+func init() {
+  tag = regexp.MustCompile(`#([a-z]+)\[|\]`)
+}
